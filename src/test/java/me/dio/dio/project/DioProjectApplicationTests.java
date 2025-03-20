8e87f19f09188c541ed31d85b6538235e922567b
David <dfawlstino@gmail.com>
2025-03-20 03:58:23 -0300

    first commit

---
+++ src/test/java/me/dio/dio/project/DioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dio.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
